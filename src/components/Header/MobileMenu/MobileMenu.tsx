431d9b1e4d1329407bdc25e29546079b281d3556
takbirgazi <takbirgazibd@gmail.com>
2025-03-28 22:19:10 +0600

    Update mobile menu

--- src/components/Header/MobileMenu/MobileMenu.tsx
+++ src/components/Header/MobileMenu/MobileMenu.tsx
@@ -19,7 +19,7 @@ const MobileMenu: React.FC<MenuType> = ({ links }) => {
                 <span className={`${isActive && "rotate-45 origin-[25%]"} border bg-white border-white w-7 h-[2px] transition-all duration-500`}></span>
                 <span className={`${isActive && "-rotate-45 origin-[25%]"} border bg-white border-white w-7 h-[2px] transition-all duration-500`}></span>
             </div>
-            <div className={`${isActive ? "h-56" : "h-0"} md:hidden flex flex-col justify-center gap-5 absolute w-full left-0 top-[115px] bg-white transition-all duration-500 overflow-hidden`}>
+            <div className={`${isActive ? "h-56 py-5" : "h-0 py-0"} md:hidden flex flex-col border-r-b-2 border-black gap-5 absolute w-full left-0 top-[115px] bg-white transition-all duration-500 overflow-hidden`}>
                 {
                     links.map(menu => <Link className="font-bold text-gray-800 text-center" href={menu.slag} key={menu.id}>{menu.title}</Link>)
                 }
