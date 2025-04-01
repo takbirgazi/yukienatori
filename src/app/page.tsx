8b9b145d111b2f59ac5b37135e635a295a422b40
takbirgazi <takbirgazibd@gmail.com>
2025-04-01 23:07:30 +0600

    Add Partner Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/HomeComp/Hero/Hero";
+import Partner from "@/components/HomeComp/Partner/Partner";
 import Services from "@/components/HomeComp/Services/Services";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <Hero />
       <div className="container mx-auto max-w-[1250px] px-4">
         <Services />
+        <Partner />
       </div>
     </div>
   );
---
+++ src/components/HomeComp/Partner/Partner.tsx
@@ -0,0 +1,31 @@
+import SectionHeader from "@/components/CommonComp/SectionHeader/SectionHeader";
+import Image from "next/image";
+import Link from "next/link";
+
+const Partner = () => {
+    const headingData = {
+        heading: "PUBLISHED IN",
+        textColor: "black"
+    };
+
+    return (
+        <div className="py-5">
+            <SectionHeader SectionHeaderData={headingData} />
+            <div>
+                <figure>
+                    <Image className="max-w-5xl mx-auto" width={1250} height={480} src="https://yukienatori-newyork.com/wp-content/uploads/2021/10/fb-1-e1628672112524-1024x358.jpg" alt="Partner" />
+                </figure>
+            </div>
+            <div className="flex flex-col gap-12 justify-center items-center pb-12">
+                <div>
+                    <Link className="px-20 uppercase hover:bg-[#8DBFBE] transition-all duration-500 py-3.5 rounded-xs bg-black text-white" href="/" >Call Us</Link>
+                </div>
+                <div>
+                    <Link className="px-20 uppercase hover:bg-[#8DBFBE] transition-all duration-500 py-3.5 rounded-xs bg-black text-white" href="/" >BOOK MASSAGE OR FACIAL ONLINE</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Partner;
