3f62ff4430a5023cd79a4edabced5dcbbf7d4a54
takbirgazi <takbirgazibd@gmail.com>
2025-04-12 00:34:56 +0600

    update for responsive

---
+++ src/components/CommonComp/ServiceCarousal/ServiceCarousalType.ts
@@ -0,0 +1,13 @@
+export interface ServiceCarousalType {
+    ServiceCarousalData: {
+        heading: string,
+        desc: string,
+        slag: string,
+        buttonData: string,
+        buttonBg: string,
+        slideImages: {
+            id: number,
+            imageUrl: string
+        }[]
+    }
+}
